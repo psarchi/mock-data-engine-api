ce6e886960c2d147d0681add46a6178a04326ea7
achik <achikochachanidze@gmail.com>
2025-09-26 23:35:26 +0400

    add (generator/leaf): enum generator

---
+++ faker_engine/generators/gens/leaf/leaf_enum.py
@@ -0,0 +1,22 @@
+from faker_engine.generators.gens.base import BaseGenerator
+from faker_engine.generators.context import GenContext
+
+
+class EnumGenerator(BaseGenerator):
+    __slots__ = 'list_of_items'
+    __aliases__ = ('enum',)
+
+    def __init__(self, list_of_items=None):
+        self.list_of_items = list_of_items
+
+    def _sanity_check(self, ctx):
+        if not self.list_of_items:
+            raise ValueError(f"No items given for for enum Generator")
+        if not isinstance(self.list_of_items, list):
+            raise ValueError(f"List given for enum Generator")
+        if not isinstance(ctx, GenContext):
+            raise TypeError("ctx must be an instance of random.Random")
+
+    def generate(self, ctx: GenContext):
+        self._sanity_check(ctx)
+        return ctx.rng.choice(self.list_of_items)
