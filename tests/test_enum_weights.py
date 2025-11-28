d1ffed14389ae76ea08160298c83ddd1ec99f6e7
Psarchi <achikochachanidze@gmail.com>
2025-11-29 00:52:28 +0400

    chore(tests): remove legacy generator unit tests

--- tests/test_enum_weights.py
+++
@@ -1,8 +0,0 @@
-from mock_engine.generators.leafs.enum import EnumGenerator
-from mock_engine.context import GenContext
-
-
-def test_enum_weights_bias():
-    ctx = GenContext(seed=3)
-    gen = EnumGenerator(values=['x', 'y'], weights=[1, 100])
-    assert gen.generate(ctx) == 'y'
