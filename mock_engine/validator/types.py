519d60d31780a672f6dcff738e8394be98763c56
Psarchi <achikochachanidze@gmail.com>
2025-12-22 23:22:56 +0400

    chore(cleanup): remove runner and validator

--- mock_engine/validator/types.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-from typing import Any, TypeVar, Mapping, Sequence, MutableMapping, MutableSequence
-
-T = TypeVar("T")
-ModelDict = Mapping[str, Any]
-MutableModelDict = MutableMapping[str, Any]
-Seq = Sequence[T]
-MutSeq = MutableSequence[T]
